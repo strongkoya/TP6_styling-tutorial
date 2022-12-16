45bc3081d19803cdc769cb29db218dc7763112e0
Toumi Mohamed Koussay <toumikoussay@hotmail.fr>
2022-12-16 22:43:01 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss'
+
+import Alert from '../Alert/Alert';
+import CartSuccess from '../CartSuccess/CartSuccess';
+
+const useStyles = createUseStyles({
+  wrapper: {
+    padding: 20,
+  }
+});
+
+function App() {
+  const classes = useStyles()
+  return(
+    <div className={classes.wrapper}>
+      <Alert title="Items Not Added" type="error">
+        <div>Your items are out of stock.</div>
+      </Alert>
+      <CartSuccess />
+    </div>
+  )
+}
+
+export default App;
